7c063940166d5bc17a03717294d9c843a42af91f
Shiva Venkatesh <shiva@bangthetable.com>
2017-11-11 11:36:39 +0530

    cleanup: code cleanup

--- public/js/create.js
+++ public/js/create.js
@@ -12,7 +12,6 @@
 	function makeAPICall(keyMappings){
 		$.post("/configuration/new", keyMappings)
 		  .done(function(response) {
-				console.log(response)
 				window.location.replace("show");
 		  });
 	}
@@ -26,14 +25,11 @@
 	function updatePossibleKeyValues(keyName){
 		possibleKeyValues.splice(possibleKeyValues.indexOf(keyName.toUpperCase()), 1);
 		possibleKeyValues.splice(possibleKeyValues.indexOf(keyName.toLowerCase()), 1);
-		console.log('array is now : ' + possibleKeyValues)		
 	}
 
 	document.addEventListener('keyup', (event) => {
 	  const keyName = event.key
 	  const keyCode = event.keyCode
-
-	  console.log('keyName value : ' + keyName)
 
 	  if(possibleKeyValues.includes(keyName)) {
 		  $(".key-preview")[0].innerHTML = keyName.toUpperCase()
@@ -42,15 +38,12 @@
 		  $(".key-preview")[3].innerHTML = keyName.toUpperCase()
 
 		  $('#jump-key').on('click', function(e) {
-				console.log('pressed JUMP')
 				keyMappings['jump'] = keyName
-				console.log(keyMappings['jump'])
 				$('.jump-select').fadeOut('slow')
 				$('.shoot-select').toggleClass('fadeIn hide')
 				clearValues()
 			})
 		  $('#shoot-key').on('click', function(e) {
-				console.log('pressed SHOOT')
 				keyMappings['shoot'] = keyName
 				$('.shoot-select').fadeOut('slow')
 				$('.slide-select').removeClass('hide')
@@ -58,23 +51,17 @@
 				clearValues()
 			})
 		  $('#slide-key').on('click', function(e) {
-				console.log('pressed SLIDE')
 				keyMappings['slide'] = keyName
 				$('.slide-select').fadeOut('slow')
 				$('.run-select').toggleClass('fadeIn hide')
 				clearValues()
 			})
 		  $('#run-key').on('click', function(e) {
-				console.log('pressed RUN')
-				$('#run-key').prop('disabled', true)
-				setTimeout(function(){
-					keyMappings['run'] = keyName
-					$('#run-key').prop('disabled', false)
-					makeAPICall(keyMappings)
-				}, 2000);
+				keyMappings['run'] = event.key
+				makeAPICall(keyMappings)
 				clearValues()
 			})
 			updatePossibleKeyValues(keyName)
 		}
-	}, false)
+	}, true)
 })();
